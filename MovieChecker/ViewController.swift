5a624db6990f3da03fd7b88b38bd0b948cb8f9c1
Yatharth Mahawar <Yatharthmahawar@outlook.com>
2020-08-18 12:43:20 +0530

    Initial Commit

---
+++ MovieChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieChecker
+//
+//  Created by Yatharth Mahawar on 18/08/20.
+//  Copyright © 2020 Yatharth Mahawar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
